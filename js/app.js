e95d329660f902915ab092dee199e00bad358127
Melyssa Nimeth <147218092+rondoElectro@users.noreply.github.com>
2025-09-04 21:57:18 -0300

    update functionality and design
    update button and display function, implemented some design improvements as well 
    *the marquee tag is temporary

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 const ipodDisplayOff = document.querySelector('.ipod-display-off');
+const ipodLoadScreen = document.querySelector('.ipod-load-screen');
 const ipodDisplayOn = document.querySelector('.ipod-display-on');
+
+let screenOn = false;
 
 const ipodCenterBtn = document.querySelector('.ipod-center-btn');
 
-function turnDisplayOn(){
-    ipodDisplayOff.style.display = 'none';
-    ipodDisplayOn.style.display = 'flex';
-}
 
 ipodCenterBtn.addEventListener('click', () => {
     ipodCenterBtn.style.background = 'linear-gradient(rgb(236, 235, 235) 20%, rgb(209, 209, 209)';
@@ -15,3 +14,23 @@ ipodCenterBtn.addEventListener('click', () => {
 ipodCenterBtn.addEventListener('mouseout', () => {
     ipodCenterBtn.style.background = 'linear-gradient(rgb(209, 209, 209) 60%, rgb(236, 235, 235))';
 })
+
+function turnLoadScreenOn(){
+    if(!screenOn){
+        ipodDisplayOff.style.display = 'none';
+        ipodLoadScreen.style.display = 'flex';
+        
+        setTimeout(() => {
+            callDisplayOn();
+        }, 15000);
+    }
+
+    screenOn = true;
+}
+
+function callDisplayOn(){
+    ipodDisplayOff.style.display = 'none';
+    ipodLoadScreen.style.display = 'none';
+    ipodDisplayOn.style.display = 'flex';
+}
+

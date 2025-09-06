d73834fa5b162afdd178b1b379c926bca18dae0f
Melyssa Nimeth <147218092+rondoElectro@users.noreply.github.com>
2025-09-05 21:38:59 -0300

    fix display bug

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+const ipodDisplayOff = document.querySelector('.ipod-display-off');
+const ipodLoadScreen = document.querySelector('.ipod-load-screen');
+const ipodDisplayOn = document.querySelector('.ipod-display-on');
+
+let screenOn = false;
+
+const ipodCenterBtn = document.querySelector('.ipod-center-btn');
+
+
+ipodCenterBtn.addEventListener('click', () => {
+    ipodCenterBtn.style.background = 'linear-gradient(rgb(236, 235, 235) 20%, rgb(209, 209, 209)';
+})
+
+ipodCenterBtn.addEventListener('mouseout', () => {
+    ipodCenterBtn.style.background = 'linear-gradient(rgb(209, 209, 209) 60%, rgb(236, 235, 235))';
+})
+
+function turnLoadScreenOn(){
+    if(!screenOn){
+        ipodDisplayOff.style.display = 'none';
+        ipodLoadScreen.style.display = 'flex';
+        
+        setTimeout(() => {
+            callDisplayOn();
+        }, 1000);
+    }
+
+    screenOn = true;
+}
+
+function callDisplayOn(){
+    ipodDisplayOff.style.display = 'none';
+    ipodLoadScreen.style.display = 'none';
+    ipodDisplayOn.style.display = 'flex';
+}
+
